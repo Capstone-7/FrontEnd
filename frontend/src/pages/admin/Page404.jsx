ab51ae735ac65db5a90000b14af268642e5ba120
ivanalrasyid <ivanalrasyid11@gmail.com>
2022-12-12 20:35:25 +0700

    feat/ivan: create 404 page

--- frontend/src/pages/admin/Page404.jsx
+++ frontend/src/pages/admin/Page404.jsx
@@ -1,24 +1,38 @@
-import { Helmet } from 'react-helmet-async';
-import { Link as RouterLink } from 'react-router-dom';
+import { Helmet } from "react-helmet-async";
+import { Link as RouterLink } from "react-router-dom";
 // @mui
-import { styled } from '@mui/material/styles';
-import { Button, Typography, Container, Box } from '@mui/material';
+import { styled } from "@mui/material/styles";
+import { Button, Typography, Container, Box } from "@mui/material";
+
+import React, { useEffect, useRef } from "react";
+import lottie from "lottie-web";
 
 // ----------------------------------------------------------------------
 
-const StyledContent = styled('div')(({ theme }) => ({
+const StyledContent = styled("div")(({ theme }) => ({
   maxWidth: 480,
-  margin: 'auto',
-  minHeight: '100vh',
-  display: 'flex',
-  justifyContent: 'center',
-  flexDirection: 'column',
+  margin: "auto",
+  minHeight: "100vh",
+  display: "flex",
+  justifyContent: "center",
+  flexDirection: "column",
   padding: theme.spacing(12, 0),
 }));
 
 // ----------------------------------------------------------------------
 
 export default function Page404() {
+  const container = useRef(null);
+
+  useEffect(() => {
+    lottie.loadAnimation({
+      container: container.current,
+      renderer: "svg",
+      loop: true,
+      autoplay: true,
+      animationData: require("./404.json"),
+    });
+  });
   return (
     <>
       <Helmet>
@@ -26,23 +40,30 @@ export default function Page404() {
       </Helmet>
 
       <Container>
-        <StyledContent sx={{ textAlign: 'center', alignItems: 'center' }}>
+        <StyledContent sx={{ textAlign: "center", alignItems: "center" }}>
           <Typography variant="h3" paragraph>
             Sorry, page not found!
           </Typography>
 
-          <Typography sx={{ color: 'text.secondary' }}>
-            Sorry, we couldn’t find the page you’re looking for. Perhaps you’ve mistyped the URL? Be sure to check your
-            spelling.
+          <Typography sx={{ color: "text.secondary" }}>
+            Sorry, we couldn’t find the page you’re looking for. Perhaps you’ve
+            mistyped the URL? Be sure to check your spelling.
           </Typography>
 
           <Box
-            component="img"
-            src="/assets/illustrations/illustration_404.svg"
-            sx={{ height: 260, mx: 'auto', my: { xs: 5, sm: 10 } }}
+            // component="img"
+            // // src="/assets/illustrations/illustration_404.svg"
+            // sx={{ height: 260, mx: "auto", my: { xs: 5, sm: 10 } }}
+            style={{ background: "no-repeat" }}
+            ref={container}
           />
 
-          <Button to="/" size="large" variant="contained" component={RouterLink}>
+          <Button
+            to="/"
+            size="large"
+            variant="contained"
+            component={RouterLink}
+          >
             Go to Home
           </Button>
         </StyledContent>
