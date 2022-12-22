069837d2ab256c95f77a8f7034bb13512dd745ac
fahd <fahderlangga1@gmail.com>
2022-12-22 16:06:53 +0700

    feat/elang: fixing product list

--- frontend/src/components/ProductList/ProductList.jsx
+++ frontend/src/components/ProductList/ProductList.jsx
@@ -113,48 +113,6 @@ const ProductList = () => {
                             )}
                         </SwiperSlide>
                     </Swiper>
-                    {/* <Carousel activeIndex={index} onSelect={handleSelect} className={styles.carousel}>
-                        <Carousel.Item>
-                            <Image
-                                className="d-block w-100"
-                                src={require("../../assets/images/daily.jpg")}
-                                alt="First slide"
-                                fluid
-                            />
-                            <Carousel.Caption>
-                                <h3>First slide label</h3>
-                                <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-                            </Carousel.Caption>
-                        </Carousel.Item>
-                        <Carousel.Item>
-                            <img
-                                className="d-block w-100"
-                                src={require("../../assets/images/bills.jpg")}
-                                alt="Second slide"
-                                fluid
-                            />
-
-                            <Carousel.Caption>
-                                <h3>Second slide label</h3>
-                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                            </Carousel.Caption>
-                        </Carousel.Item>
-                        <Carousel.Item>
-                            <img
-                                className="d-block w-100"
-                                src={require("../../assets/images/entertainment.jpg")}
-                                alt="Third slide"
-                                fluid
-                            />
-
-                            <Carousel.Caption>
-                                <h3>Third slide label</h3>
-                                <p>
-                                    Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-                                </p>
-                            </Carousel.Caption>
-                        </Carousel.Item>
-                    </Carousel> */}
                 </Col>
             </Row>
         </Container>
